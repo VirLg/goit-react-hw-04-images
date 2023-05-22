0cd36103ddec028df3e7bcfb1c9e190886b87c90
VirLg <19virlg@gmail.com>
2023-05-23 00:26:20 +0300

    add

--- src/components/ImageItem/ImageItem.jsx
+++ src/components/ImageItem/ImageItem.jsx
@@ -1,93 +1,110 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import ModalWindow from '../ModalWindow/ModalWindov';
 import { ImageGalleryItem } from './ImageItem.styled';
+//* Hooks
 
-class ImageItem extends Component {
-  state = {
-    images: null,
-    activeCard: null,
-    toggleSwitch: false,
+const ImageItem = props => {
+  const { card } = props;
+  // useState
+  const [images, setImages] = useState(null);
+  const [activCard, setActivCard] = useState(null);
+  const [toggleSwitcher, setToggleSwitcher] = useState(false);
+  // useState
+
+  const toggleModal = () => {
+    setToggleSwitcher(!setToggleSwitcher);
   };
 
-  toggleModal = () => {
-    this.setState({ toggleSwitch: !this.state.toggleSwitch });
-  };
+  useEffect(() => {
+    if (images !== card) setImages(card);
+  });
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.images !== this.props.card) {
-      console.log(this.state.images);
-      this.setState(() => ({ images: this.props.card }));
-    }
-  }
-
-  handleActivCard = largeImageURL => {
+  function handleActivCard(largeImageURL) {
     if (largeImageURL) {
-      this.setState({ activeCard: largeImageURL });
-      this.toggleModal();
+      setActivCard(largeImageURL);
+
+      toggleModal();
     }
 
     console.log(largeImageURL);
     return largeImageURL;
-  };
-
-  render() {
-    return (
-      <>
-        {this.state.images &&
-          this.state.images.map(
-            ({ id, previewURL, tags, largeImageURL }, idx) => (
-              <ImageGalleryItem className="gallery__item" key={idx}>
-                <img
-                  src={previewURL}
-                  alt={tags}
-                  onClick={() => this.handleActivCard(largeImageURL)}
-                />
-              </ImageGalleryItem>
-            )
-          )}
-
-        {this.state.toggleSwitch && (
-          <ModalWindow>
-            <img src={this.state.activeCard} alt="" />
-          </ModalWindow>
-        )}
-      </>
-    );
   }
-}
+
+  return (
+    <>
+      {images &&
+        images.map(({ id, previewURL, tags, largeImageURL }, idx) => (
+          <ImageGalleryItem className="gallery__item" key={idx}>
+            <img
+              src={previewURL}
+              alt={tags}
+              onClick={() => handleActivCard(largeImageURL)}
+            />
+          </ImageGalleryItem>
+        ))}
+
+      {toggleSwitcher && (
+        <ModalWindow>
+          <img src={activCard} alt="" />
+        </ModalWindow>
+      )}
+    </>
+  );
+};
+
+// Class
+// class ImageItem extends Component {
+//   state = {
+//     images: null,
+//     activeCard: null,
+//     toggleSwitch: false,
+//   };
+
+//   toggleModal = () => {
+//     this.setState({ toggleSwitch: !this.state.toggleSwitch });
+//   };
+
+//   componentDidUpdate(prevProps, prevState) {
+//     if (prevState.images !== this.props.card) {
+//       console.log(this.state.images);
+//       this.setState(() => ({ images: this.props.card }));
+//     }
+//   }
+
+//   handleActivCard = largeImageURL => {
+//     if (largeImageURL) {
+//       this.setState({ activeCard: largeImageURL });
+//       this.toggleModal();
+//     }
+
+//     console.log(largeImageURL);
+//     return largeImageURL;
+//   };
+
+//   render() {
+//     return (
+//       <>
+//         {this.state.images &&
+//           this.state.images.map(
+//             ({ id, previewURL, tags, largeImageURL }, idx) => (
+//               <ImageGalleryItem className="gallery__item" key={idx}>
+//                 <img
+//                   src={previewURL}
+//                   alt={tags}
+//                   onClick={() => this.handleActivCard(largeImageURL)}
+//                 />
+//               </ImageGalleryItem>
+//             )
+//           )}
+
+//         {this.state.toggleSwitch && (
+//           <ModalWindow>
+//             <img src={this.state.activeCard} alt="" />
+//           </ModalWindow>
+//         )}
+//       </>
+//     );
+//   }
+// }
 
 export default ImageItem;
-
-// const onClick=(evt)=>{
-
-//     console.log(evt);
-// }
-
-// class ItemImage extends React.Component{
-
-// handleClick = (options) =>{
-// const{images}=options
-// this.setState({images})
-
-// console.log(images);
-// }
-
-//   render(){
-//     return(<>
-//      {this.state.images&&this.state.images.map(({ id, previewURL, tags }) => (
-//   <li key={id}>
-//     <img src={previewURL} alt={tags} />
-//   </li>))
-
-//   }
-//   </>)
-// }
-// }
-
-// export default ItemImage
-
-// {images.map(({ id, previewURL, tags }) => (
-//   <li key={id}>
-//     <img src={previewURL} alt={tags} onClick={this.handleClick}/>
-//   </li>
-// ))}
--- src/components/ModalWindow/ModalWindov.jsx
+++ src/components/ModalWindow/ModalWindov.jsx
@@ -1,43 +1,82 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+
 import { ModalBackdrop, ModalItem } from './ModalWindow.styled';
+// *Hook
 
-class ModalWindow extends React.Component {
-  state = {
-    close: true,
-  };
+const ModalWindow = ({ children }) => {
+  const [close, setClose] = useState(true);
 
-  componentDidMount() {
+  useEffect(() => {
     document.addEventListener('keydown', this.handlePressEscape);
-  }
+  }, []);
 
-  componentWillUnmount() {
-    document.removeEventListener('keydown', this.handlePressEscape);
-  }
-  handlePressEscape = e => {
+  useEffect(() => {
+    document.removeEventListener('keydown', handlePressEscape);
+  });
+
+  const handlePressEscape = e => {
     console.log('press');
-    if (e.code === 'Escape') this.setState({ close: false });
+    if (e.code === 'Escape') setClose(false);
   };
 
-  handleClick = e => {
+  const handleClick = e => {
     console.log(e);
     if (e.target === e.currentTarget) {
-      this.setState({ close: false });
+      setClose(false);
     }
   };
 
-  render() {
-    return (
-      <>
-        {this.state.close && (
-          <ModalBackdrop className="Modal__Backdrop" onClick={this.handleClick}>
-            <ModalItem className="Modal__Item">
-              <div>{this.props.children}</div>
-            </ModalItem>
-          </ModalBackdrop>
-        )}
-      </>
-    );
-  }
-}
+  return (
+    <>
+      {close && (
+        <ModalBackdrop className="Modal__Backdrop" onClick={handleClick}>
+          <ModalItem className="Modal__Item">
+            <div>{children}</div>
+          </ModalItem>
+        </ModalBackdrop>
+      )}
+    </>
+  );
+};
+
+//*Class
+// class ModalWindow extends React.Component {
+//   state = {
+//     close: true,
+//   };
+
+//   componentDidMount() {
+//     document.addEventListener('keydown', this.handlePressEscape);
+//   }
+
+//   componentWillUnmount() {
+//     document.removeEventListener('keydown', this.handlePressEscape);
+//   }
+//   handlePressEscape = e => {
+//     console.log('press');
+//     if (e.code === 'Escape') this.setState({ close: false });
+//   };
+
+//   handleClick = e => {
+//     console.log(e);
+//     if (e.target === e.currentTarget) {
+//       this.setState({ close: false });
+//     }
+//   };
+
+//   render() {
+//     return (
+//       <>
+//         {this.state.close && (
+//           <ModalBackdrop className="Modal__Backdrop" onClick={this.handleClick}>
+//             <ModalItem className="Modal__Item">
+//               <div>{this.props.children}</div>
+//             </ModalItem>
+//           </ModalBackdrop>
+//         )}
+//       </>
+//     );
+//   }
+// }
 
 export default ModalWindow;
