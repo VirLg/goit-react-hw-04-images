c2d3bf8f68751b0797b8add5ad079fc559c65559
VirLg <19virlg@gmail.com>
2023-05-22 23:20:49 +0300

    app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 import SearchBar from './components/SearchBar/SearchBar';
@@ -9,27 +10,20 @@ import ImageGallery from 'components/ImageGallery/ImageGallery';
 
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '34368263-756a5eb3a3e360b335b61bac8';
-
-class App extends React.Component {
-  state = {
-    searchText: '',
-    images: [],
-    loading: false,
-    error: null,
-    page: 1,
-    buttonVisible: false,
-  };
-
-  async componentDidUpdate(prevProps, prevState) {
-    const { searchText, page } = this.state;
-
-    if (prevState.searchText !== searchText || prevState.page !== page) {
-      this.setState({
-        buttonVisible: false,
-      });
-
+// Hook
+const App = () => {
+  //*useState
+  const [searchText, setSearchText] = useState('');
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [page, setPage] = useState(1);
+  const [buttonVisible, setButtonVisible] = useState(false);
+  //*useState
+  useEffect(
+    () => async () => {
       try {
-        this.setState({ loading: true });
+        setLoading(true);
         const data = await fetch(
           `${BASE_URL}/?key=${API_KEY}&q=${searchText}&per_page=12&page=${page}`
         );
@@ -37,59 +31,141 @@ class App extends React.Component {
           return Promise.reject(new Error('Search is empty'));
         }
         const resp = await data.json();
-        return await this.getFetch(resp);
+        return await getFetch(resp);
       } catch (error) {
-        this.setState({ error });
+        setError(error);
       } finally {
-        this.setState({ loading: false });
+        setLoading(false);
       }
-    }
-  }
-  handleOnClick = () => {
+    },
+    [page, searchText]
+  );
+  // setButtonVisible(false);
+  // async componentDidUpdate(prevProps, prevState) {
+  //   const { searchText, page } = this.state;
+
+  //   if (prevState.searchText !== searchText || prevState.page !== page) {
+
+  //   }
+  // }
+  const handleOnClick = () => {
     console.log(1);
   };
 
-  heandleSearch = searchText => {
-    this.setState({
-      searchText: searchText,
-      images: [],
-    });
+  const heandleSearch = searchText => {
+    setSearchText(searchText);
+    setImages([]);
   };
-  getFetch = ({ hits, totalHits }) => {
+  const getFetch = ({ hits, totalHits }) => {
     const hitsArr = hits.map(({ tags, largeImageURL, previewURL }) => {
       return { tags, largeImageURL, previewURL };
     });
 
-    this.setState(prevState => ({ images: [...prevState.images, ...hitsArr] }));
-    if (totalHits === 0)
-      this.setState({
-        images: [],
-      });
-    if (totalHits > 1)
-      this.setState({
-        buttonVisible: true,
-      });
+    setImages(prevState => [...prevState, ...hitsArr]);
+
+    if (totalHits === 0) setImages([]);
+
+    if (totalHits > 1) setButtonVisible(true);
   };
 
-  handleLoadMore = onClick => {
+  const handleLoadMore = onClick => {
     console.log(onClick);
     console.log(handleOnClick());
-    console.log(this.handleLoadMore);
+    console.log(handleLoadMore);
   };
 
-  render() {
-    const { error, loading, images, buttonVisible } = this.state;
+  return (
+    <AppDiv className="App">
+      <SearchBar onSubmit={heandleSearch} />
+      {loading && <div>загружаем</div>}
+      {error && <h1>{error.message}</h1>}
+      <ImageGallery options={images} />
+      {buttonVisible && <Button page={handleLoadMore} />}
+    </AppDiv>
+  );
+};
 
-    return (
-      <AppDiv className="App">
-        <SearchBar onSubmit={this.heandleSearch} />
-        {loading && <div>загружаем</div>}
-        {error && <h1>{error.message}</h1>}
-        <ImageGallery options={images} />
-        {buttonVisible && <Button page={this.handleLoadMore} />}
-      </AppDiv>
-    );
-  }
-}
+//Class
+// class App extends React.Component {
+//   state = {
+//     searchText: '',
+//     images: [],
+//     loading: false,
+//     error: null,
+//     page: 1,
+//     buttonVisible: false,
+//   };
+
+//   async componentDidUpdate(prevProps, prevState) {
+//     const { searchText, page } = this.state;
+
+//     if (prevState.searchText !== searchText || prevState.page !== page) {
+//       this.setState({
+//         buttonVisible: false,
+//       });
+
+//       try {
+//         this.setState({ loading: true });
+//         const data = await fetch(
+//           `${BASE_URL}/?key=${API_KEY}&q=${searchText}&per_page=12&page=${page}`
+//         );
+//         if (data.status !== 200) {
+//           return Promise.reject(new Error('Search is empty'));
+//         }
+//         const resp = await data.json();
+//         return await this.getFetch(resp);
+//       } catch (error) {
+//         this.setState({ error });
+//       } finally {
+//         this.setState({ loading: false });
+//       }
+//     }
+//   }
+//   handleOnClick = () => {
+//     console.log(1);
+//   };
+
+//   heandleSearch = searchText => {
+//     this.setState({
+//       searchText: searchText,
+//       images: [],
+//     });
+//   };
+//   getFetch = ({ hits, totalHits }) => {
+//     const hitsArr = hits.map(({ tags, largeImageURL, previewURL }) => {
+//       return { tags, largeImageURL, previewURL };
+//     });
+
+//     this.setState(prevState => ({ images: [...prevState.images, ...hitsArr] }));
+//     if (totalHits === 0)
+//       this.setState({
+//         images: [],
+//       });
+//     if (totalHits > 1)
+//       this.setState({
+//         buttonVisible: true,
+//       });
+//   };
+
+//   handleLoadMore = onClick => {
+//     console.log(onClick);
+//     console.log(handleOnClick());
+//     console.log(this.handleLoadMore);
+//   };
+
+//   render() {
+//     const { error, loading, images, buttonVisible } = this.state;
+
+//     return (
+//       <AppDiv className="App">
+//         <SearchBar onSubmit={this.heandleSearch} />
+//         {loading && <div>загружаем</div>}
+//         {error && <h1>{error.message}</h1>}
+//         <ImageGallery options={images} />
+//         {buttonVisible && <Button page={this.handleLoadMore} />}
+//       </AppDiv>
+//     );
+//   }
+// }
 
 export default App;
