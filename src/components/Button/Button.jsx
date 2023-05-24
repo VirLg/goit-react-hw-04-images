28a8f08415365bcb0909b23d3e524a3f3e8e49ef
VirLg <19virlg@gmail.com>
2023-05-24 17:29:01 +0300

    add

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,34 +1,6 @@
 import { ButtonCss } from './Button.styled';
-// class Button extends Component{
-// state={
-//     page:2,
-// }
-
-// increment=()=>{
-//     this.setState(()=>({
-//         page:this.state.page +1,
-//     }))
-//     this.handleOnClick(this.state.page)
-// }
-
-// handleOnClick=()=>{
-
-//     this.props.page(this.state.page)
-// }
-
-// render(){
-//     return(
-//         <>
-//     <ButtonCss type="button" onClick={this.increment} >Load More</ButtonCss>
-//     </>
-//     )
-// }
-// }
 
 const Button = function (props) {
-  // const hadleLoadMore = () => {
-  //   console.log();
-  // };
   return (
     <>
       <ButtonCss type="button" onClick={props.page}>
